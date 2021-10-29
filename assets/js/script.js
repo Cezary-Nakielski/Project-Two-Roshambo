ca14b2d6ec4f92db858a25103f8ecf361987e079
Cezary-Nakielski <cezarnakielski@gmail.com>
2021-10-29 10:13:21 +0000

    FEATURE: Add a function which determines the computers choice

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,6 @@
 
-// Variable needed for the event listener loop
 const buttons = document.getElementsByClassName("choice-button");
 
-/**
- * Event listener loop for all interactive buttons
- */
 for (let button of buttons) {
     button.addEventListener("click", function() {
        let playerChoice = this.getAttribute("data-choice");
@@ -23,7 +19,8 @@ for (let button of buttons) {
     });
 }
 
-// Functions passing data from event listeners to the function comparing the player and computer choices
+
+
 function rockData() {
     choiceResultData = "fas fa-hand-rock";
 }
@@ -34,4 +31,94 @@ function paperData() {
 
 function scissorsData() {
     choiceResultData = "fas fa-hand-scissors";
+}
+
+
+
+
+
+
+function robotChoice() {
+    let robotNumber = Math.floor(Math.random()*3);
+
+    if (robotNumber === 0) {
+        document.getElementById("robot-choice").setAttribute("class", "fas fa-hand-rock");
+    } else if (robotNumber === 1) {
+        document.getElementById("robot-choice").setAttribute("class", "fas fa-hand-paper");
+    } else {
+        document.getElementById("robot-choice").setAttribute("class", "fas fa-hand-scissors");
+    } 
+    choiceResult();
+}
+
+
+function choiceResult() {
+    let playerResult = choiceResultData;
+    let robotResult = document.getElementById("robot-choice").getAttribute("class");
+   
+    if (playerResult !== robotResult) {
+        if (playerResult === "fas fa-hand-rock") {
+            if (robotResult === "fas fa-hand-scissors") {
+                win();
+            } else {
+                defeat();
+            }
+        } else if (playerResult === "fas fa-hand-paper") {
+            if (robotResult === "fas fa-hand-rock") {
+                win();
+            } else {
+                defeat();
+            }
+        } else if (playerResult === "fas fa-hand-scissors") {
+            if (robotResult === "fas fa-hand-paper") {
+                win();
+            } else {
+                defeat();
+            }
+        } 
+    } else {
+        draw();
+    }
+}
+
+function resetGame() {
+    document.getElementById("robot-choice").setAttribute("class", "fas fa-robot");
+    document.getElementsByClassName("player-score").innerText = 0;
+    document.getElementsByClassName("computer-score").innerText = 0;
+}
+
+function win() {
+    let playerScore = parseInt(document.getElementsByClassName("player-score").innerText);
+    playerScore++;
+    document.getElementsByClassName("player-score").innerText = playerScore;
+    setTimeout(function(){ swal({
+        title: "Win",
+        icon: "success",
+        button: false,
+        timer: 1500,
+      }); }, 100);
+}
+
+
+function defeat() {
+    let computerScore = parseInt(document.getElementsByClassName("computer-score").innerText);
+    computerScore++;
+    document.getElementsByClassName("computer-score").innerText = computerScore;
+    setTimeout(function(){ swal({
+        title: "Defeat",
+        icon: "error",
+        button: false,
+        timer: 1500,
+      }); }, 100);
+}
+
+
+
+function draw() {
+    setTimeout(function(){ swal({
+        title: "Draw",
+        icon: "info",
+        button: false,
+        timer: 1500,
+      }); }, 100);
 }
